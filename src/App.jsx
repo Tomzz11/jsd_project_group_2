3f83b07a3a6eaaa02466cf932314cad5fb02ffef
Kroekpol noonpang <kroekpol.cpk@gmail.com>
2026-01-14 16:41:19 +0700

    update checkout and payment

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,26 @@ import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
 import Layout from './pages/Layout';
 import UserDashboard from './components/dashboard/UserDashboard';
-import OrderHistory  from './pages/OrderHistory';
+import OrderHistory from './pages/OrderHistory';
 import Checkout from './pages/Checkout';
-
+import Payment from './pages/Payment'; 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      {path: "/", element: <Home /> ,},
-      {path:"Cart", element: <Cart />},
-      {path:"Products", element: <Products />},
-      {path:"Productdetail", element: <ProductDetail />},
-      {path:"UserDashboard", element: <UserDashboard />},
-      {path:"OrderHistory", element:<OrderHistory/>},
-      {path:"Checkout", element:<Checkout/>},
-
+      {path: "/", element: <Home />},
+      {path: "Cart", element: <Cart />},
+      {path: "Products", element: <Products />},
+      {path: "Productdetail", element: <ProductDetail />},
+      {path: "UserDashboard", element: <UserDashboard />},
+      {path: "OrderHistory", element: <OrderHistory />},
+      {path: "Checkout", element: <Checkout />},
+      {path: "Payment", element: <Payment />},
     ]
   }
 ]);
-
-
-
-
-
 
 function App() {
   return (
@@ -39,4 +34,3 @@ function App() {
 }
 
 export default App;
-
